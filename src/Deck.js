476918861bf5e758460001913917a28efe78f148
Ryan <ryanhuebert@gmail.com>
2017-07-08 07:45:36 -0700

    Create panHandler to handle all  cards

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import {
+  View,
+  Animated,
+  PanResponder
+ } from 'react-native';
 
 class Deck extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY();
+    // Never updated with setState() could have:
+    // this.panResponder = panResponder;
+    // this.state = { panResponder };
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        this.position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
@@ -10,9 +28,12 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <Animated.View
+      style={this.position.getLayout()}
+      {...this.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
